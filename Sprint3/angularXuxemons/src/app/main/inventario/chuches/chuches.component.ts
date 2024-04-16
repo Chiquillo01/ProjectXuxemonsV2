4a582d2cbaf4d402837d3f12084f53f182c60184
Edgar <edgarchb44@gmail.com>
2024-04-16 22:20:49 +0200

    creación con comandos de los componentes echos hasta el momento

---
+++ Sprint3/angularXuxemons/src/app/main/inventario/chuches/chuches.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chuches',
+  templateUrl: './chuches.component.html',
+  styleUrls: ['./chuches.component.css']
+})
+export class ChuchesComponent {
+
+}
